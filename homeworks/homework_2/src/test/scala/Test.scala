25a18941f03ebba600e5e451afe63987dc060c26
ShooT-DD <44630185+ShooT-DD@users.noreply.github.com>
2020-10-22 14:48:12 +0500

    Revert "homework_2 - Давыдов Денис"
    This reverts commit 868cf43f1b0a1675649822bc1fa34bb91758e338.

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,5 +1,4 @@
 import utest._
-import Exercises.Vector2D
 
 object Test extends TestSuite{
 
@@ -9,27 +8,5 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
-        'test_sumOfDivBy3Or5 - {
-            assert(Exercises.sumOfDivBy3Or5(1,9) == 23)
-            assert(Exercises.sumOfDivBy3Or5(1,3) == 3)
-            assert(Exercises.sumOfDivBy3Or5(1,5) == 8)
-        }
-        'test_primeFactor - {
-            assert(Exercises.primeFactor(80) == Seq(2,5))
-            assert(Exercises.primeFactor(98) == Seq(2,7))
-            assert(Exercises.primeFactor(13) == Seq(13))
-        }
-        'test_sumScalars - {
-            assert(Exercises.sumScalars(Vector2D(2.0, 2.0), Vector2D(2.0, 2.0), Vector2D(2.0, 2.0), Vector2D(2.0, 2.0)) == 16)
-            assert(Exercises.sumScalars(Vector2D(0.0, 0.0), Vector2D(0.0, 0.0), Vector2D(0.0, 0.0), Vector2D(0.0, 0.0)) == 0)
-        }
-        'test_sumCosines - {
-            assert(Exercises.sumCosines(Vector2D(2.0, 2.0), Vector2D(2.0, 2.0), Vector2D(3.0, 2.0), Vector2D(3.0, 2.0)) == 2.0)
-            assert(Exercises.sumCosines(Vector2D(1.0, 0.0), Vector2D(1.0, 0.0), Vector2D(2.0, 0.0), Vector2D(2.0, 0.0)) == 2)
-        }
-        'test_sortByHeavyweight - {
-            assert(Exercises.sortByHeavyweight() == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium", "Lead", "Sodium", "Uranium", "Gold", "Tungsten", "Zirconium", "Chrome", "Iron", "Copper", "Silver", "Plutonium", "Cobalt", "Cesium", "Calcium", "Lithium", "Magnesium", "Potassium", "Graphite"))
-        }
     }
 }
-
