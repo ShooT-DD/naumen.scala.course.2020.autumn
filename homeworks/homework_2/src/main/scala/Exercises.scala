b869d5044f5a5b7036ef870d0a0a822156d994c6
ShooT-DD <44630185+ShooT-DD@users.noreply.github.com>
2020-10-22 14:49:19 +0500

    Revert "Revert "homework_2 - Давыдов Денис""
    This reverts commit 25a18941f03ebba600e5e451afe63987dc060c26.

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -1,3 +1,5 @@
+import scala.collection.SortedMap
+
 object Exercises {
 
     /*ПРИМЕР*/
@@ -16,16 +18,52 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        var result: Long = 0
+        for (i <- iFrom to iTo){
+             if(i % 3 == 0 || i % 5 == 0) {
+                result += i
+            }
+              }
+        result
+    }
 
-
+    def primeNum(n: Int): Int = {
+        var b = 1
+        var i = n-1;
+        while(i != 1) {
+            if(n%i == 0){
+                b = n/i
+                return b
+            }
+            i -= 1
+        }
+        b
+    }
 
     /*ЗАДАНИЕ II*/
     /*Реализовать функцию, которая вычисляет все различные простые множители целого числа отличные от 1.
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        var a: Int = 2
+        var n = number
+        var res: Seq[Int] = Seq()
+        while(a != 1){
+            a = primeNum(n)
+            n = n/a
+            if(a == 1){
+                if(!res.contains(n) && n != 1) {
+                    res = res :+ n
+                }
+            }
+            if(!res.contains(a) && a != 1) {
+                res = res :+ a
+            }
+        }
+        res
+    }
 
 
 
@@ -40,15 +78,17 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, function: (Vector2D,Vector2D)=>Double, rightVec0: Vector2D, rightVec1: Vector2D): Double = {
+        function(leftVec0, leftVec1) + function(rightVec0, rightVec1)
+    }
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
+
 
 
 
@@ -71,6 +111,10 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def WeightBall(a:(Int, Double)): Double = (4/3 * java.lang.Math.PI * Math.pow(a._1,3)) * a._2
 
+
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        ballsArray.mapValues(i => WeightBall(i._1,i._2)).toList.sortBy(_._2).map(_._1)
+    }
 }
